d774499569747f25b3607ba5f171387053a9e123
Faraz khan <farazp096@gmail.com>
2023-05-03 00:22:42 +0530

    eslint fixed

--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react'
-import { render, cleanup, screen } from '@testing-library/react'
+import { render, cleanup } from '@testing-library/react'
 
 import { PhoneInput } from '../src/components/PhoneInput'
 
-// afterEach function runs after each test suite is executed
 afterEach(() => {
-  cleanup() // Resets the DOM after each test suite
+  cleanup()
 })
 
 describe('Phone input', () => {
